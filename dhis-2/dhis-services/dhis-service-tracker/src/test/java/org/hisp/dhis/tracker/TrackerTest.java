a740db9caf76d0db7e6a822075c1c4a81067f428
teleivo <teleivo@users.noreply.github.com>
2022-04-05 17:04:26 +0200

    chore: translate tracker JSON fixture in TrackerTest

--- dhis-2/dhis-services/dhis-service-tracker/src/test/java/org/hisp/dhis/tracker/TrackerTest.java
+++ dhis-2/dhis-services/dhis-service-tracker/src/test/java/org/hisp/dhis/tracker/TrackerTest.java
@@ -30,6 +30,7 @@ package org.hisp.dhis.tracker;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -141,7 +142,14 @@ public abstract class TrackerTest extends TransactionalIntegrationTest
     protected TrackerImportParams _fromJson( String path )
         throws IOException
     {
-        return renderService.fromJson( new ClassPathResource( path ).getInputStream(), TrackerImportParams.class );
+        TrackerImportParams params = renderService.fromJson( new ClassPathResource( path ).getInputStream(),
+            TrackerImportParams.class );
+        // TODO(DHIS2-12563) remove once migration is done
+        try ( FileOutputStream out = new FileOutputStream( "src/test/resources/" + path ) )
+        {
+            renderService.toJson( out, params );
+        }
+        return params;
     }
 
     protected void assertNoImportErrors( TrackerImportReport report )
